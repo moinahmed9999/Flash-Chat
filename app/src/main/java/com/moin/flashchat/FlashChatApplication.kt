d080759d246312fff82c3461970025dd0b481bf5
Moin Ahmed <moinahmed9999@gmail.com>
2021-01-29 01:59:22 +0530

    Add check for sign in with google if email has been signed up or not

--- app/src/main/java/com/moin/flashchat/FlashChatApplication.kt
+++ app/src/main/java/com/moin/flashchat/FlashChatApplication.kt
@@ -8,6 +8,6 @@ class FlashChatApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 }
--- app/src/main/java/com/moin/flashchat/data/repository/LogInRepository.kt
+++ app/src/main/java/com/moin/flashchat/data/repository/LogInRepository.kt
@@ -9,14 +9,15 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.moin.flashchat.extension.await
 import com.moin.flashchat.utils.Result
 
 class LogInRepository {
     private var auth = Firebase.auth
-//    private val db = Firebase.firestore
-//    private val collection = db.collection(USER_COLLECTION_NAME)
+    private val db = Firebase.firestore
+    private val collection = db.collection(USER_COLLECTION_NAME)
 
     val snackBar = MutableLiveData<String?>()
 
@@ -50,21 +51,27 @@ class LogInRepository {
     suspend fun logInUserWithGoogle(task: Task<GoogleSignInAccount>) {
         try {
             val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
-            account?.let {
-                val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
-                when(val result = auth.signInWithCredential(credential).await()) {
-                    is Result.Success -> {
-                        Log.d(TAG, "logInUserWithGoogle: Sign In Successful ${result.data?.user?.displayName} ${result.data.user?.phoneNumber}")
-                        snackBar.value = "Sign In ${result.data?.user?.displayName} ${result.data.user?.phoneNumber}"
-                        _signIn.value = true
-                    }
-                    is Result.Error -> {
-                        Log.e(TAG, "logInUserWithGoogle: ${result.exception.message}")
-                        snackBar.value = result.exception.message
-                    }
-                    is Result.Canceled -> {
-                        Log.e(TAG, "logInUserWithGoogle: ${result.exception?.message}")
-                        snackBar.value = "Result Canceled"
+            account?.let { account ->
+                account.email?.let { email ->
+                    if (doesUserExists(email)) {
+                        val credential: AuthCredential = GoogleAuthProvider.getCredential(account.idToken, null)
+                        when(val result = auth.signInWithCredential(credential).await()) {
+                            is Result.Success -> {
+                                Log.d(TAG, "logInUserWithGoogle: Sign In Successful ${result.data?.user?.displayName} ${result.data.user?.phoneNumber}")
+                                snackBar.value = "Sign In ${result.data?.user?.displayName} ${result.data.user?.phoneNumber}"
+                                _signIn.value = true
+                            }
+                            is Result.Error -> {
+                                Log.e(TAG, "logInUserWithGoogle: ${result.exception.message}")
+                                snackBar.value = result.exception.message
+                            }
+                            is Result.Canceled -> {
+                                Log.e(TAG, "logInUserWithGoogle: ${result.exception?.message}")
+                                snackBar.value = "Result Canceled"
+                            }
+                        }
+                    } else {
+                        snackBar.value = "Account with this email address does not exist"
                     }
                 }
             }
@@ -75,8 +82,30 @@ class LogInRepository {
         }
     }
 
+    private suspend fun doesUserExists(email: String): Boolean {
+        when (val result = collection.whereEqualTo("email", email).get().await()) {
+            is Result.Success -> {
+                val size = result.data.documents.size
+                if (size > 0) {
+                    Log.d(TAG, "doesUserExists: true")
+                    return true
+                }
+            }
+            is Result.Error -> {
+                Log.e(TAG, "doesUserExists: ${result.exception.message}")
+                snackBar.value = result.exception.message
+            }
+            is Result.Canceled -> {
+                Log.e(TAG, "doesUserExists: ${result.exception?.message}")
+                snackBar.value = "Result Canceled"
+            }
+        }
+        Log.d(TAG, "doesUserExists: false")
+        return false
+    }
+
     companion object {
         private const val TAG = "LogInRepository"
-//        private const val USER_COLLECTION_NAME = "users"
+        private const val USER_COLLECTION_NAME = "users"
     }
 }
